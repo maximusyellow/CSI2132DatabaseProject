8a46608534e0bf1ad06f4b1b265829a3269edfc4
maximusyellow <yerrowranger@gmail.com>
2020-04-07 13:35:37 -0400

    finish draft
    issue with host property insert
    
    -issue with heating parameter - psql query does not see the value inserted, a static value is not seen as well
    
    -issue with country, foreign key does not exist

--- OnlineTravel.sh
+++ OnlineTravel.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-$ export LC_ALL=
 ACT[0]="Find Properties"
 ACT[1]="Host Login"
 ACT[2]="Employee Login"
@@ -45,15 +44,17 @@ ACT_0()
 # Host Login
 ACT_1()
 {
-  output='false'
+  userid=''
   read -p "Email Address (example@example.com): " host_email_input
   export PGPASSWORD='Gvasn2v2bj' #your postgres password
-  psql -h 127.0.0.1 -d postgres -U postgres -c "SELECT EXISTS (SELECT sys_user.* FROM \"OnlineTravel\".sys_user WHERE email LIKE '%$host_email_input%')"
+  userid=`psql -X -h 127.0.0.1 -d postgres -U postgres -t -c "SELECT sys_user.userid FROM \"OnlineTravel\".sys_user WHERE email LIKE '%$host_email_input%'"`
+  echo "Found userid: $userid"
   option=''
-  if [[ $output == *'f'* ]]; then
-    echo "Welcome '$host_email_input'"
+  if [[ $userid != '' ]]; then
+    echo "Welcome $host_email_input"
     read -p 'Would you like to make a listing [Y/N]? ' option
     if [[ $option == "Y" ]]; then
+      propertyid=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 10 | head -n 1)
       read -p "Per Night Fee $" per_night_fee
       read -p "Name of Property " name
       read -p "Max Number of Guests " max_guests
@@ -64,13 +65,16 @@ ACT_1()
       read -p "City " city
       read -p "Province " province
       read -p "Country " country
-      read -p "Entire Home? (true/false) " entire_home
-      read -p "Spark Clean? (true/false) " spark_clean
-      read -p "Heating? (true/false) " heating
+      read -p "Entire Home? (t/f) " entire_home
+      read -p "Spark Clean? (t/f) " spark_clean
+      read -p "heating? (t/f) " heating
       read -p "Description " description
       export PGPASSWORD='Gvasn2v2bj' #your postgres password
-      psql -h 127.0.0.1 -d postgres -U postgres -c "INSERT INTO \"OnlineTravel\".property(pernightfee, name, maxguests, numbathrooms, housenumber, street, postalcode, city, province, country, entirehome, sparkclean, wifi, heating, description) VALUES ('$per_night_fee', '$name', '$max_guests', '$num_bathrooms', '$house_number', '$street', '$postal_code', '$city', '$province', '$country', '$entire_home', '$spark_clean', '$wifi', '$heating', '$description');"
+      psql -h 127.0.0.1 -d postgres -U postgres -c "INSERT INTO \"OnlineTravel\".property(propertyid, userid, pernightfee, name, maxguests, numbathrooms, housenumber, street, postalcode, city, province, country, entirehome, sparkclean, wifi, heating, description) 
+      VALUES ( '$propertyid', '$userid', '$per_night_fee', '$name', $max_guests, $num_bathrooms, $house_number, '$street', '$postal_code', '$city', '$province', '$country', '$entire_home', '$spark_clean', '$wifi', '$heating', '$description');"
     fi
+  else
+    echo "The email specified does not exist in the system, please try again"
   fi
 }
 
@@ -85,12 +89,10 @@ ACT_3()
   read -p "Specify Branch Country: " country_input
   export PGPASSWORD='Gvasn2v2bj' #your postgres password
   psql -h 127.0.0.1 -d postgres -U postgres -c "\encoding UTF8;" -c "SELECT property.propertyid, booking_info.bookingid, booking_info.bookerid, property.name, property.maxguests, property.pernightfee, property.city, startdate, enddate FROM \"OnlineTravel\".property natural join \"OnlineTravel\".booking_info WHERE country LIKE '%$country_input%' and (startdate > '$current_date' ) ORDER BY startdate ASC;"
-  read -p 'If you would like to see more information on property (including past and future bookings), please enter the propertyid [N to cancel]'
+  read -p 'If you would like to see more information on property (including past and future bookings), please enter the propertyid [N to cancel] '
   echo $option
-  if [[$option == "N"]]; then
-    continue
-  else
-  psql -h 127.0.0.1 -d postgres -U postgres -c "\encoding UTF8;" -c "SELECT property.*, startdate, enddate FROM \"OnlineTravel\".property natural join \"OnlineTravel\".booking_info WHERE propertyid LIKE '%$option%';"
+  if [[ $option != "N" ]]; then
+    psql -h 127.0.0.1 -d postgres -U postgres -c "\encoding UTF8;" -c "SELECT property.*, startdate, enddate FROM \"OnlineTravel\".property natural join \"OnlineTravel\".booking_info WHERE propertyid LIKE '%$option%';"
   fi
 
 }
@@ -103,25 +105,25 @@ ACT_4()
   read -p "Specify Branch Country: " country_input
   export PGPASSWORD='Gvasn2v2bj' #your postgres password
   psql -h 127.0.0.1 -d postgres -U postgres -c "SELECT property.propertyid, booking_info.bookingid, booking_info.bookerid, property.name, property.maxguests, property.pernightfee, property.city, startdate, enddate FROM \"OnlineTravel\".property natural join \"OnlineTravel\".booking_info WHERE country LIKE '%$country_input%' and (enddate < '$current_date' or startdate > '$current_date') ORDER BY enddate ASC;"
-  read -p 'If you would like to see more information on property (including past and future bookings), please enter the propertyid [N to cancel]' option
+  read -p 'If you would like to see more information on property (including past and future bookings), please enter the propertyid [N to cancel] ' option
   echo $option
-  if [[$option == "N"]]; then
-    continue
-  else
+  if [[ $option != "N" ]]; then
     psql -h 127.0.0.1 -d postgres -U postgres -c "\encoding UTF8;" -c "SELECT property.*, startdate, enddate FROM \"OnlineTravel\".property natural join \"OnlineTravel\".booking_info WHERE propertyid LIKE '%$option%';"
   fi
 }
 #Employee Login
 ACT_2()
 {
+  employeeid=''
   read -p "Employee Email Address (example@example.com): " employee_email_input
   export PGPASSWORD='Gvasn2v2bj' #your postgres password
-  psql -h 127.0.0.1 -d postgres -U postgres -c "\encoding UTF8;" -c "SELECT EXISTS (SELECT employee.* FROM \"OnlineTravel\".employee WHERE email LIKE '%$employee_email_input%')"
+  employeeid=`psql -X -h 127.0.0.1 -d postgres -U postgres -t -c "\encoding UTF8;" -c "SELECT employee.employeeid FROM \"OnlineTravel\".employee WHERE email LIKE '%$employee_email_input%'"`
   output='f'
   option=''
-  if [[ $output == *'f'* ]]; then
-    echo "Welcome '$employee_email_input'"
-    echo "Here are your options:"
+  echo "Found employeeid: $employeeid"
+  if [[ $employeeid != '' ]]; then
+    echo "Welcome $employee_email_input"
+    echo "Here are your options: "
     LOC=2
     while [ true ]; do
       LEGAL_ACTS=$(echo "${LOC_ACTS[$LOC]}" | sed 's|,|\n|g')
@@ -142,6 +144,8 @@ ACT_2()
       fi
       ACT_${ACT_INPUT}
     done
+  else
+    echo "The email specified does not exist in the system, please try again"
   fi
 }
 
